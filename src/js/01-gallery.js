d40b4c95252731f0fea48d6416b5cc686bdd8e77
HannaNikola <h.nikolaienko32@gmail.com>
2023-07-19 23:23:44 +0200

    create-prodject

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
 // Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+
+
+
+// import { galleryItems } from './gallery-items.js';
+// // Change code below this line
+// console.log(galleryItems);
+
+// const imgEl = document.querySelector('.gallery');
+// console.log(imgEl);
+
+// const imagesList = galleryItems
+//   .map(item => {
+//     return ` <li class="gallery__item">
+//    <a class="gallery__link" href="${item.original}">
+//       <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+//    </a>
+// </li>
+//         `;
+//   })
+//   .join('');
+
+// imgEl.insertAdjacentHTML('beforeend', imagesList);
+
+// imgEl.addEventListener('click', showPhoto);
+
+// function showPhoto(event) {
+//   event.preventDefault();
+//   console.log(event.target.tagName);
+//   if (event.target.tagName !== 'IMG') {
+//     return item.original;
+//   }
+// }
+
+// const lightbox = new SimpleLightbox('.gallery a ', {
+//   captionsData: 'alt',
+//   captionDelay: 250,
+// });
