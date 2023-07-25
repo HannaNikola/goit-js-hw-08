e11ca9651767b8866ef182679e8ea82809293648
HannaNikola <h.nikolaienko32@gmail.com>
2023-07-25 19:07:53 +0200

    update-task-3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,27 @@
+// import _ from 'lodash';
+
+const formElement = document.querySelector('.feedback-form');
+const emailElement = document.querySelector('input');
+const massageElement = document.querySelector('textarea');
+const buttonElement = document.querySelector('button');
+// console.log(emailElement);
+// console.log(massageElement);
+// console.log(formElement);
+// console.log(buttonElement);
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+let inputObject = {};
+
+formElement.addEventListener('input', onInput);
+
+
+
+function onInput(event) {
+  inputObject[event.target.name] = event.target.value;
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputObject));
+}
+
+function saveMassage(evt) {
+  evt.preventDefault();
+}
 
