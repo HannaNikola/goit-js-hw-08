9440e5d989edf8339c9635f35689b04e7a40f220
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2023-07-25 17:08:24 +0000

    Deploying to gh-pages from @ HannaNikola/goit-js-hw-08@e11ca9651767b8866ef182679e8ea82809293648 🚀

---
+++ 03-feedback.df344fb8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form"),t=(document.querySelector("input"),document.querySelector("textarea"),document.querySelector("button"),{});e.addEventListener("input",(function(e){t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))}))}();
+//# sourceMappingURL=03-feedback.df344fb8.js.map
