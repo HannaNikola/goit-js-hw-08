0d57329c0bbb9f5df65b34b9344a99d2b44f009a
HannaNikola <h.nikolaienko32@gmail.com>
2023-07-27 00:40:03 +0200

    update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,24 +4,47 @@ const formElement = document.querySelector('.feedback-form');
 const emailElement = document.querySelector('input');
 const massageElement = document.querySelector('textarea');
 const buttonElement = document.querySelector('button');
-// console.log(emailElement);
-// console.log(massageElement);
-// console.log(formElement);
-// console.log(buttonElement);
+
 
 const LOCALSTORAGE_KEY = 'feedback-form-state';
 let inputObject = {};
 
 formElement.addEventListener('input', onInput);
+formElement.addEventListener('submit', onSubmit);
 
 
 
 function onInput(event) {
   inputObject[event.target.name] = event.target.value;
   localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(inputObject));
+
+
 }
 
-function saveMassage(evt) {
-  evt.preventDefault();
+ function onSubmit (event) {
+    event.preventDefault();
+    if (inputObject.email && inputObject.message) {
+      console.log(inputObject);
+      alert('Данні успішно відправленні');
+      formElement.reset();
+      localStorage.removeItem(LOCALSTORAGE_KEY);
+      // inputObject = {};
+    } else {
+      alert(' Поля повинні бути заповнені');
+    }
 }
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
 
