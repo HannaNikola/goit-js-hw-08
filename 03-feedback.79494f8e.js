76af390799d3b6130f0064db002cb6738f2d4469
HannaNikola <111384616+HannaNikola@users.noreply.github.com>
2023-07-27 00:37:38 +0000

    Deploying to gh-pages from @ HannaNikola/goit-js-hw-08@a3837cd97692b404443b5acd5ddf64514dc05c2a 🚀

--- 03-feedback.79494f8e.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".feedback-form");document.querySelector("input"),document.querySelector("textarea"),document.querySelector("button");let t={};e.addEventListener("input",(function(e){t[e.target.name]=e.target.value,localStorage.setItem("feedback-form-state",JSON.stringify(t))})),e.addEventListener("submit",(function(r){r.preventDefault(),t.email&&t.message?(console.log(t),alert("Данні успішно відправленні"),e.reset(),localStorage.removeItem("feedback-form-state"),t={}):alert(" Поля повинні бути заповнені")}));
-//# sourceMappingURL=03-feedback.79494f8e.js.map
