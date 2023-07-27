c94ea0ace62c9e50a302f89c371165f8aa7f151e
HannaNikola <h.nikolaienko32@gmail.com>
2023-07-28 01:18:11 +0200

    update-task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -8,32 +8,30 @@ const LOCALSTORAGE_KEY = 'videoplayer-current-time';
 
 
 
-const onPlay = function ({seconds}) {
-  localStorage.setItem(LOCALSTORAGE_KEY, seconds );
-  
+const onPlay = function ({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
 };
-player.on('timeupdate', onPlay);
+player.on('timeupdate', throttle(onPlay, 1000)); 
 
 
 
 
-// player
-//   .setCurrentTime({seconds})
-//   .then(function (seconds) {
-//     // seconds = the actual time that the player seeked to
-//   })
-//   .catch(function (error) {
-//     switch (error.name) {
-//       case 'RangeError':
-//         // the time was less than 0 or greater than the video’s duration
-//         break;
+player
+  .setCurrentTime(30.456)
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
 
-//       default:
-//         // some other error occurred
-//         break;
-//     }
-//   });
-
+      default:
+        // some other error occurred
+        break;
+    }
+  });
 
 
 
